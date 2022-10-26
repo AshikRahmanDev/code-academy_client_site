4876cfc930ae21e4fad6a707f5fc6df381d039a3
MohammdAshik <mohammadashik797@gmail.com>
2022-10-26 23:47:20 +0600

    install firebase and create firebase.config.js and export app

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcbVhv2LnURZ-JuUBQT03NaeT53KQa6TI",
+  authDomain: "elearning-auth-433fc.firebaseapp.com",
+  projectId: "elearning-auth-433fc",
+  storageBucket: "elearning-auth-433fc.appspot.com",
+  messagingSenderId: "324204554278",
+  appId: "1:324204554278:web:b436aac9253f92d15c9ee7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
