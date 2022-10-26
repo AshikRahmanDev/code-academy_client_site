cd54a02abe61029006f0719f50f7612cca58f8e8
MohammdAshik <mohammadashik797@gmail.com>
2022-10-26 18:38:52 +0600

    using loader for fetch data from backend

--- src/components/CoursesCard.jsx
+++ src/components/CoursesCard.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const CoursesCard = ({ course }) => {
   const { picture } = course;
   return (
-    <div className="div flex h-[210px] bg-base-100 shadow-xl rounded-xl">
+    <div className="h-[210px] bg-base-100 shadow-xl rounded-lg p-1">
       <figure>
-        <img src={picture} alt="Movie" />
+        <img
+          className="w-[100%] h-[170px] rounded-lg"
+          src={picture}
+          alt="Movie"
+        />
       </figure>
       <div className="card-body">
         <h2 className="card-title">New movie is released!</h2>
-        <p>Click the button to watch on Jetflix app.</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Watch</button>
-        </div>
       </div>
     </div>
   );
